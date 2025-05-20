b051699f77dfbadce496983f87b805610483a8b7
Hassan Mesghali <hassan.mesghali@gmail.com>
2025-05-20 09:44:39 +0330

    Add on_startup functionality in which, database and its models are created

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from sqlmodel import SQLModel
+
+from app.core.db import engine
 
 app = FastAPI()
 
@@ -6,3 +9,9 @@ app = FastAPI()
 @app.get('/')
 def hello_world():
 	return {'message': 'hello world'}
+
+
+@app.on_event(event_type='startup')
+def on_startup():
+	SQLModel.metadata.drop_all(engine)
+	SQLModel.metadata.create_all(engine)
