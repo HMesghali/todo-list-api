31bd9aef0e15383fea6cd0fda1a518b227013dc8
Hassan Mesghali <hassan.mesghali@gmail.com>
2025-05-20 10:22:52 +0330

    Add dependencies that are injected in APIs
    - Add session dependency
    - Add getting user dependency

---
+++ app/api/deps.py
@@ -0,0 +1,56 @@
+from collections.abc import Generator
+from typing import Annotated
+
+import jwt
+from core.db import engine
+from core.security import SECRET_KEY, SECURITY_ALGORITHM
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jwt.exceptions import InvalidTokenError
+from pydantic import ValidationError
+from sqlmodel import Session
+
+from app.models import TokenPayload, User
+
+reusable_oath2 = OAuth2PasswordBearer(tokenUrl='token')
+
+
+def get_db() -> Generator[Session, None, None]:
+	with Session(engine) as session:
+		yield session
+
+
+TokenDep = Annotated[str, Depends(reusable_oath2)]
+SessionDep = Annotated[Session, Depends(get_db)]
+
+
+def get_current_user(session: SessionDep, token: TokenDep) -> User:
+	try:
+		payload = jwt.decode(
+			token, secret_key=SECRET_KEY, algorithms=SECURITY_ALGORITHM
+		)
+		token_data = TokenPayload(**payload)
+	except (InvalidTokenError, ValidationError):
+		raise HTTPException(
+			status_code=status.HTTP_403_FORBIDDEN,
+			detail='Could not validate credentials',
+		)
+	user = session.get(User, token_data.sub)
+	if not user:
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+	if not user.is_active:
+		raise HTTPException(
+			status_code=status.HTTP_400_BAD_REQUEST, detail='Inactive user!'
+		)
+	return user
+
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
+
+
+def get_current_active_superuser(current_user: CurrentUser) -> User:
+	if not current_user.is_superuser:
+		raise HTTPException(
+			status_code=403, detail="The user doesn't have enough privileges"
+		)
+	return current_user
