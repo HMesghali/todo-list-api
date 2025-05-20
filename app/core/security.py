a52590b93799e9ec85f9aa64069303206efe6e82
Hassan Mesghali <hassan.mesghali@gmail.com>
2025-05-20 10:35:19 +0330

    Create access token func to security section

--- app/core/security.py
+++ app/core/security.py
@@ -1,3 +1,20 @@
+from datetime import UTC, datetime, timedelta
+from typing import Any
+
+import jwt
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
 SECRET_KEY = 'bdafebc1513573af9e56b278ac1200758f0bdf8485bcb95ec6fa59ea14f74e05'
 SECURITY_ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
+	expire = datetime.now(UTC) + expires_delta
+	to_encode = {'exp': expire, 'sub': str(subject)}
+	encoded_jwt = jwt.encode(
+		to_encode, SECRET_KEY, algorithm=SECURITY_ALGORITHM
+	)
+	return encoded_jwt
