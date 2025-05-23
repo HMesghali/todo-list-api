b632457832d619b9ef9addd71cdda349ea829064
Hassan Mesghali <hassan.mesghali@gmail.com>
2025-05-23 09:47:08 +0330

    Add login functionality which returns jwt token to a valid user.

--- app/api/routes/login.py
+++ app/api/routes/login.py
@@ -1,3 +1,37 @@
-from fastapi import APIRouter
+from datetime import timedelta
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app import crud
+from app.api.deps import SessionDep
+from app.core.security import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
+from app.models import Token
 
 router = APIRouter(tags=['login'])
+
+
+@router.post('/login/access-token')
+def login_access_token(
+	*,
+	session: SessionDep,
+	form_date: Annotated[OAuth2PasswordRequestForm, Depends()],
+):
+	user = crud.authenticate(
+		session=session, email=form_date.username, password=form_date.password
+	)
+	if not user:
+		raise HTTPException(
+			status_code=status.HTTP_404_NOT_FOUND,
+			detail='Incorrect email or password',
+		)
+	elif not user.is_active:
+		raise HTTPException(
+			status_code=status.HTTP_400_BAD_REQUEST, detail='Inactive user'
+		)
+	return Token(
+		access_token=create_access_token(
+			user.id, expires_delta=timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
+		)
+	)
--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,33 +1,27 @@
 from typing import Any
 
 from fastapi import APIRouter, HTTPException, status
-from sqlmodel import select
 
+from app import crud
 from app.api.deps import SessionDep
-from app.core.security import get_password_hash
-from app.models import User, UserCreate, UserPublic
+from app.models import UserCreate, UserPublic
 
 router = APIRouter(prefix='/users', tags=['users'])
 
 
 @router.post(
-	'/', status_code=status.HTTP_201_CREATED, response_model=UserPublic
+	'/',
+	status_code=status.HTTP_201_CREATED,
+	response_model=UserPublic,
 )
 def create_user(*, session: SessionDep, user_in: UserCreate) -> Any:
 	# check whether the user with this email exist or not
-	membership_statement = select(User).where(User.email == user_in.email)
-	user = session.exec(statement=membership_statement).first()
+	user = crud.get_user_by_email(session=session, email=user_in.email)
 	if user:
 		raise HTTPException(
 			status_code=status.HTTP_400_BAD_REQUEST,
 			detail='The user with this email already exists!',
 		)
 
-	user = User.model_validate(
-		user_in,
-		update={'hashed_password': get_password_hash(user_in.password)},
-	)
-	session.add(user)
-	session.commit()
-	session.refresh(user)
+	user = crud.create_user(session=session, user_create=user_in)
 	return user
