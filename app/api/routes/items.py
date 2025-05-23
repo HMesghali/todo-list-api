fbad32dc680a5f6a356849da865a5d4c77e7eab9
Hassan Mesghali <hassan.mesghali@gmail.com>
2025-05-23 14:28:27 +0330

    Add update api for todos

--- app/api/routes/items.py
+++ app/api/routes/items.py
@@ -1,9 +1,10 @@
+import uuid
 from typing import Any
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, HTTPException, status
 
 from app.api.deps import CurrentUser, SessionDep
-from app.models import Item, ItemCreate, ItemPublic
+from app.models import Item, ItemCreate, ItemPublic, ItemUpdate
 
 router = APIRouter(prefix='/todos', tags=['todos'])
 
@@ -23,6 +24,32 @@ def create_todo(
 
 
 # update a todo
+@router.patch(
+	'/{id}', status_code=status.HTTP_200_OK, response_model=ItemPublic
+)
+def update_todo(
+	*,
+	session: SessionDep,
+	current_user: CurrentUser,
+	id: uuid.UUID,
+	item_in: ItemUpdate,
+) -> Any:
+	item = session.get(Item, id)
+	if not item:
+		raise HTTPException(
+			status_code=status.HTTP_404_NOT_FOUND, detail='Item not found!'
+		)
+	if current_user.id != item.owner_id:
+		raise HTTPException(
+			status_code=status.HTTP_400_BAD_REQUEST, detail='Not allowed!'
+		)
+	update_dict = item_in.model_dump(exclude_unset=True)
+	item.sqlmodel_update(update_dict)
+	session.add(item)
+	session.commit()
+	session.refresh(item)
+	return item
+
 
 # delete a todo
 
