70dbdeb8f96a764b6bfb048371856e00d921a719
Hassan Mesghali <hassan.mesghali@gmail.com>
2025-06-11 09:01:47 +0330

    Add scalar documentation to the project which can be used instead of postman. check 'scalar.com' for further information if needed. Note that scalar-fastapi should be installed

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
 from sqlmodel import SQLModel
 
 from app import models  # noqa: F401
@@ -11,6 +12,13 @@ app = FastAPI()
 @app.get('/')
 def hello_world():
 	return {'message': 'hello world'}
+
+
+@app.get('/scalar', include_in_schema=False)
+def get_scalar_docs():
+	return get_scalar_api_reference(
+		openapi_url=app.openapi_url, title='Scalar Documentation API'
+	)
 
 
 @app.on_event(event_type='startup')
